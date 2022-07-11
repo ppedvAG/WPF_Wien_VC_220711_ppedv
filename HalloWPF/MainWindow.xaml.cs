1e1fe10a467eaa5103845618fa72bfbd201d295f
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-07-11 14:29:00 +0200

    Add project files.

---
+++ HalloWPF/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HalloWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //Dies verweist auf eine Methode in der (versteckten) automatisch generierten zweiten Klassen-Datei (*.g.i.cs),
+            //welche für das Rendering des XAML-Codes verantwortlich ist. InitializeComponent() erstellt die
+            //Steuerelement-Objekte und muss daher als erste Methode des Konstruktors bestehen bleiben
+            InitializeComponent();
+        }
+
+        private void Btn_BeispielButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Verändern des Inhalts des Buttons
+            Btn_BeispielButton.Content = "Ich wurde angeklickt";
+            //Ändern einer UI-Property über den Sender-Parameter (beinhaltet den Verursacher des Events)
+            (sender as Button).Content = "Ich wurde angeklickt";
+        }
+    }
+}
